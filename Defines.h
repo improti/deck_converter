45b9fd8cd0b2be36315f1a99ce9882a44b019126
improti <tim@dirasad.de>
2016-12-09 21:43:55 +0100

    Added comments and paths where necessary

--- Defines.h
+++ Defines.h
@@ -1,7 +1,14 @@
 #define RELEASE
 #define CONVERT
 
+// Path to the images as used by the client
+#define PICTURE_DIRECTORY "/path/to/pictures"
+// Path to the database
+#define DATABASEDIRECTORY "/path/to/database"
+
+// If defined, the converter will automatically replace
+// all images of standard lands with those of the edition
+// specified here.
 #define LANDEDITION "UNH"
-#define PICTURE_DIRECTORY 
-#define DEBUGDECK 
-#define DATABASEDIRECTORY
+
+#define DEBUGDECK "debugdeck"
